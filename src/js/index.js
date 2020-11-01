c49cdd43b3d9832e91d33d3a6bbd50152d0b803e
pashakiz <pashakiz@gmail.com>
2020-11-01 08:37:13 +0300

    remove trash

--- src/js/index.js
+++ src/js/index.js
@@ -16,20 +16,6 @@ $(function() {
             }
         }, 1000);
 
-        //mobile menu expand
-        $('.header').on('click', '.header__nav-toggle', function(){
-            let header_menu = $(this).closest('.header').find('.header-menu');
-            if ( $('.header').hasClass('expand') ) {
-                header_menu.slideUp(300);
-                setTimeout(function() {
-                    $('.header').removeClass('expand');
-                }, 300);
-            } else {
-                header_menu.slideDown(300);
-                $('.header').addClass('expand');
-            }
-        });
-
         //upload photo
         $('.custom-file-input').on('change', function(){
             let imageUrl = 'img/photos/ava.jpg';
